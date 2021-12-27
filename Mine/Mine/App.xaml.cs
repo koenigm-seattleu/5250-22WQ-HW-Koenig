96138c1bbe2c43302f4164b88732d769fd71f3e1
koenigm-seattleu <koenigm@seattleu.edu>
2021-12-27 11:35:56 +0900

    Adding initial files to project

--- Mine/Mine/App.xaml.cs
+++ Mine/Mine/App.xaml.cs
@@ -1,15 +1,19 @@
 ﻿using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Mine.Services;
+using Mine.Views;
 
 namespace Mine
 {
     public partial class App : Application
     {
+
         public App()
         {
             InitializeComponent();
 
+            DependencyService.Register<MockDataStore>();
             MainPage = new MainPage();
         }
 
